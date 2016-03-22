1627ecc29a06fdada1b5d91a092d9d911355d483
deabo <aluno@gsortlab01>
2016-03-21 21:06:11 -0300

    primeiro django

---
+++ aulaDjango/aulaDjango/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/aluno/jr_inf627/inf627_exercicios/aulaDjango/aulaDjango/bin/python3.4
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
