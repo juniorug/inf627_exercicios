fd438edbceca0982a1800588f261716558012f5e
juniorug <juniorug@gmail.com>
2016-03-27 14:33:34 +0000

    terminando display

--- Adafruit_CharLCD_IPclock_example.py
+++ Adafruit_CharLCD_IPclock_example.py
@@ -9,7 +9,7 @@ lcd = Adafruit_CharLCD()
 
 cmd = "ip addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1"
 
-lcd.begin(16, 1)
+lcd.begin(16, 2)
 
 
 def run_cmd(cmd):
@@ -20,9 +20,12 @@ def run_cmd(cmd):
 def __del__(self):
     lcd.clear()
 
+
+ipaddr = run_cmd(cmd)
+lcd.message('%s ' % (ipaddr))
+
 for i in range(10, -1, -1):
-    lcd.clear()
-    ipaddr = run_cmd(cmd)
-    lcd.message('%s %s' % (ipaddr, i))
+    lcd.setCursor(0,1)
+    lcd.message('%s ' % ( i ))
     sleep(1)
 lcd.clear()
