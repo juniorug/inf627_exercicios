35fbcbcf6df41d03ced54598be28fdf0117b1148
juniorug <juniorug@gmail.com>
2016-03-29 15:47:59 -0300

    alterando modelo de dados

--- ddlSqlite3.sql
+++ ddlSqlite3.sql
@@ -2,10 +2,18 @@
 -- Table `mydb`.`sensor_family`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sensor_family` (
-  `id` INT NOT NULL,
+  `id` INTEGER PRIMARY KEY NOT NULL,
   `family_name` VARCHAR(45) NULL,
-  `default_measure_unit` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
+  `default_measure_unit` VARCHAR(45) NULL
+);
+
+-- -----------------------------------------------------
+-- Table `mydb`.`place`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `place` (
+  `id` INTEGER PRIMARY KEY NOT NULL,
+  `description` VARCHAR(45) NULL
+);
   
 -- -----------------------------------------------------
 -- Table `mydb`.`sensor`
@@ -13,20 +21,16 @@ CREATE TABLE IF NOT EXISTS `sensor_family` (
 CREATE TABLE IF NOT EXISTS `sensor` (
   `id` INT NOT NULL,
   `sensor_family_id` INT NOT NULL,
+  `place_id` INT NOT NULL, 
   `sensor_type` VARCHAR(45) NULL,
+  `pin` INT NOT NULL,
+  `sampling_time` INT NOT NULL,  
   `status` VARCHAR(45) NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (`sensor_family_id`) REFERENCES sensor_family (`id`) 
+  FOREIGN KEY (`sensor_family_id`) REFERENCES sensor_family (`id`) ,
+  FOREIGN KEY (`place_id`)  REFERENCES `place` (`id`)
 );
-  
--- -----------------------------------------------------
--- Table `mydb`.`place`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `place` (
-  `id` INT NOT NULL,
-  `description` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
-  
+    
 -- -----------------------------------------------------
 -- Table `mydb`.`sensor_measurement`
 -- -----------------------------------------------------
@@ -35,6 +39,11 @@ CREATE TABLE IF NOT EXISTS `sensor_measurement` (
   `sensor_id` INT NULL,
   `datetime_measurement` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `value` VARCHAR(45) NULL,
-  `place_id` INT NULL,
-  FOREIGN KEY (`sensor_id`) REFERENCES `sensor` (`id`),
-  FOREIGN KEY (`place_id`)  REFERENCES `place` (`id`));
+  FOREIGN KEY (`sensor_id`) REFERENCES `sensor` (`id`)
+);
+-- -----------------------------------------------------
+-- drop table sensor_family;
+-- drop table sensor;
+-- drop table place;
+-- drop table sensor_measurement;
+-- -----------------------------------------------------
--- dmlSqlite3.sql
+++ dmlSqlite3.sql
@@ -3,16 +3,17 @@ VALUES (1, 'Temperature', '°C'),
        (2, 'Humidity', '%'),
        (3, 'Luminosity', 'lx');
 
-
-INSERT INTO sensor (id, sensor_family_id, sensor_type, status)
-VALUES (1, 1, 'DHT11Temperture', 1),
-       (2, 2, 'DHT11Humidity, 1),
-       (3, 3, 'LDR', 1);
-
-
 INSERT INTO place (id, description)
 VALUES (1, 'center inside');
 
-"""INSERT INTO sensor_measurement (sensor_id, datetime_measurement, place_id,value)
-VALUES (?, ?, ?, ?);""",())
+INSERT INTO sensor (id, sensor_family_id,place_id, sensor_type,pin,sampling_time, status)
+VALUES (1, 1, 1, 'DHT11Temperture', 4, 5, 1),
+       (2, 2, 1, 'DHT11Humidity', 4, 5, 1),
+       (3, 3, 1, 'LDR', 18, 5, 1);
 
+-- -----------------------------------------------------
+-- select * from sensor_family;
+-- select * from sensor;
+-- select * from place;
+-- select * from sensor_measurement;
+-- -----------------------------------------------------
