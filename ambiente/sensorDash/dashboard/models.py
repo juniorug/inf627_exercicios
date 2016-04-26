f4cecaf5c7b670cc9bac081b7a4b5ca76d2dd078
Adewale Andrade <adewale.andrade@gmail.com>
2016-04-26 00:45:56 -0300

    adding reader thread call on sensor save method

--- ambiente/sensorDash/dashboard/models.py
+++ ambiente/sensorDash/dashboard/models.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime
 from django.db import models
 from django.utils.translation import gettext as _
+from reader import SensorReader
 # from django.utils import timezone
 
 
@@ -16,18 +17,24 @@ class SensorFamily(models.Model):
 class Sensor(models.Model):
 
     sensor_family = models.ForeignKey('SensorFamily')
-    sensor_type = models.CharField(max_length = 45)
+    sensor_type = models.CharField(max_length=45)
     place = models.ForeignKey('Place')
-    sampling_time = models.IntegerField(default = 1)
-    pin = models.IntegerField(default = 0)
-    status = models.CharField(max_length = 45)
+    sampling_time = models.IntegerField(default=1)
+    pin = models.IntegerField(default=0)
+    status = models.CharField(max_length=45)
 
     def __str__(self):
         return self.sensor_type + ' - ' + self.status
 
     def save(self, *args, **kwargs):
         super(Sensor, self).save(*args, **kwargs)
-        # print self.sensor_type
+
+        # Identify the sensor and call it's reader thread
+        readerType = SensorFamily.objects.get(
+            self.sensor_family_id).values('family_name')
+        reader = SensorReader.createReader(
+            readerType, self.sensor_type, self.sampling_time, self.id)
+        reader.startThread()
 
 
 class Place(models.Model):
@@ -42,17 +49,15 @@ class Place(models.Model):
 class SensorMeasurement(models.Model):
 
     sensor = models.ForeignKey('Sensor')
-    date_measurement = models.DateField(_("Date"), default= date.today)
+    date_measurement = models.DateField(_("Date"), default=date.today)
     time_measurement = models.TimeField(blank=True, null=True)
     value = models.CharField(max_length=45)
 
     def __str__(self):
-        return self.value + ' at ' + unicode(self.date_measurement) + ' ' + unicode(self.time_measurement)
+        return self.value + ' at ' + unicode(self.date_measurement)\
+            + ' ' + unicode(self.time_measurement)
 
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
         self.datetime_measurement = timezone.now()
         return super(User, self).save(*args, **kwargs)
-
-
-
